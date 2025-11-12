f2c1821a1040be29ee8965b156b5d5ea8ada9c82
Sakshiarote <arotesakshi02@gmail.com>
2025-11-12 08:11:56 +0530

    Update expression(post order traversal).cpp

--- expression(post order traversal).cpp
+++ expression(post order traversal).cpp
@@ -2,56 +2,48 @@
 #include <cstring>
 using namespace std;
 
-struct Node {
-    char d;
-    Node *l, *r;
-};
+struct Node { char d; Node *l,*r; };
 
-Node* st[30]; 
-int top = -1;
-void push(Node* n){ st[++top] = n; }
-Node* pop(){ return st[top--]; }
-
-Node* makeTree(char p[]) {
-    for (int i = strlen(p)-1; i >= 0; i--) {
-        Node* n = new Node{p[i], NULL, NULL};
-        if (isalpha(p[i])) push(n);
-        else {
-            n->l = pop();
-            n->r = pop();
-            push(n);
-        }
+Node* build(const char* p){
+    Node* s[30]; int t=-1;
+    for(int i=strlen(p)-1;i>=0;i--){
+        Node* n=new Node{p[i],0,0};
+        if(isalpha(p[i])) s[++t]=n;
+        else { n->l=s[t--]; n->r=s[t--]; s[++t]=n; }
     }
-    return pop();
+    return s[t];
 }
 
-void post(Node* r) {
-    if(!r) return;
-    Node* s1[30]; int t1=-1;
-    Node* s2[30]; int t2=-1;
+void inorder(Node* r){ if(!r) return; inorder(r->l); cout<<r->d; inorder(r->r); }
 
+void postNR(Node* r){
+    if(!r) return;
+    Node *s1[30],*s2[30]; int t1=-1,t2=-1;
     s1[++t1]=r;
-    while(t1!=-1){
-        Node* x=s1[t1--];
-        s2[++t2]=x;
-        if(x->l) s1[++t1]=x->l;
-        if(x->r) s1[++t1]=x->r;
+    while(t1>=0){
+        Node* c=s1[t1--];
+        s2[++t2]=c;
+        if(c->l) s1[++t1]=c->l;
+        if(c->r) s1[++t1]=c->r;
     }
-    while(t2!=-1) cout << s2[t2--]->d;
+    cout<<"Postorder: ";
+    while(t2>=0) cout<<s2[t2--]->d;
 }
 
-void del(Node* r){
+void delTree(Node* r){
     if(!r) return;
-    del(r->l);
-    del(r->r);
-    cout << "\nDeleting: " << r->d;
+    delTree(r->l);
+    delTree(r->r);
+    cout<<"\nDeleting: "<<r->d;
     delete r;
 }
 
-int main() {
-    char p[20];
-    cin >> p;
-    Node* root = makeTree(p);
-    post(root);
-    del(root);
+int main(){
+    char pre[30];
+    cout<<"Enter prefix: ";
+    cin>>pre;
+    Node* t=build(pre);
+    cout<<"Inorder: "; inorder(t);
+    cout<<endl; postNR(t);
+    delTree(t);
 }
