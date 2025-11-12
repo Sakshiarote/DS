ba99bd1ce16f9be9cf89e1c30939bebaaf6305c8
Sakshiarote <arotesakshi02@gmail.com>
2025-11-12 18:09:19 +0530

    Update linear search.py

--- linear search.py
+++ linear search.py
@@ -1,11 +1,46 @@
-customer_ids = [102, 205, 150, 123, 301, 110, 175]
+customer_ids = []
 
 def linear_search(customer_list, target_id):
-    for i in range(len(customer_list)):
-        if customer_list[i] == target_id:
+    for cid in customer_list:
+        if cid == target_id:
             return True
     return False
 
-search_id = int(input("Enter Customer ID to search: "))
-found_linear = linear_search(customer_ids, search_id)
-print("Linear Search: Found" if found_linear else "Linear Search: Not Found")
+while True:
+    print("\n---- E-Commerce Customer Account Menu ----")
+    print("1. Add Customer Account IDs")
+    print("2. Display All Customer Account IDs")
+    print("3. Search for a Customer ID (Linear Search)")
+    print("4. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        n = int(input("Enter number of customers to add: "))
+        for i in range(n):
+            cid = int(input(f"Enter Customer ID {i+1}: "))
+            customer_ids.append(cid)
+        print("Customer IDs added successfully!")
+
+    elif choice == 2:
+        if not customer_ids:
+            print("No customer IDs available.")
+        else:
+            print("\nCustomer Account IDs:")
+            for cid in customer_ids:
+                print(cid)
+
+    elif choice == 3:
+        if not customer_ids:
+            print("No customer data available for search.")
+        else:
+            search_id = int(input("Enter Customer ID to search: "))
+            found = linear_search(customer_ids, search_id)
+            print("Linear Search: Found" if found else "Linear Search: Not Found")
+
+    elif choice == 4:
+        print("Exiting program... Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
